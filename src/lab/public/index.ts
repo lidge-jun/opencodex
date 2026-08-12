71ae65d86d397e2c8afb67d86b005f72390378d6
Wibias <37517432+Wibias@users.noreply.github.com>
2026-08-12 20:51:28 +0200

    feat(lab): expose CL-10 public bundle APIs

--- src/lab/public/index.ts
+++ src/lab/public/index.ts
@@ -3,3 +3,6 @@ export * from "./ids";
 export * from "./registry";
 export * from "./validate";
 export * from "./project";
+export * from "./bundle";
+export * from "./signature";
+export * from "./storage";
